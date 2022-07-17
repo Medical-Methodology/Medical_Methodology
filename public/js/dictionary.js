347c022dfa0ce2f49d9e2f7359b0b88e029f494b
Withanage Perera <prav.perera@gmail.com>
2022-07-16 23:52:40 -0400

    More comments.

--- public/js/dictionary.js
+++ public/js/dictionary.js
@@ -5,7 +5,7 @@ const listTerms = document.querySelector('#list');
 const loginButton = document.querySelector('#loginButton');
 const signupButton = document.querySelector('#signupButton');
 
-// Commonly used injected HTML
+// Injectable HTML for filter-by-letter buttons
 var searchTools =
 `
     <div style = 'display: flex; justify-content: center;'>
@@ -24,6 +24,7 @@ var searchTools =
         <button style = 'font-size: 15px; border: black solid; border-width: 1px; background-color: yellow; padding: 3px; margin-right: 3px;' onclick = sort("M")>M</button>
     </div>
 
+    <!-- Spacer -->
     <div style = 'padding: 5px;'></div>
 
     <div style = 'display: flex; justify-content: center;'>
@@ -42,16 +43,21 @@ var searchTools =
         <button style = 'font-size: 15px; border: black solid; border-width: 1px; background-color: yellow; padding: 3px; margin-right: 3px;' onclick = sort("Z")>Z</button>
     </div>
 
+    <!-- Spacer -->
     <div style = 'padding: 5px;'></div>
 
+    <!-- Reset Button -->
     <div>
         <button style = 'font-size: 15px; border: black solid; border-width: 1px; background-color: yellow; padding: 3px; margin-right: 3px;' onclick = sort("reset")>Reset</button>
     </div>
 
     <h1 style = 'font-size: 25px; padding-top: 10px; padding-bottom: 5px;'>Search</h1>
+
+    <!-- Search bar -->
     <form action = 'dictionary.html' method = 'POST' id = 'search-bar' style = 'padding-bottom: 15px;'>
         <input type = 'text' name = 'search' id = 'search' style = 
         '
+            <!-- Element styling -->
             font-size: 18px;
             border: black solid;
             border-width: 3px;
@@ -62,16 +68,20 @@ var searchTools =
         </input>
     </form>
 
+    <!-- Creates div that will be filled with a list of terms, div will be closed later -->
     <div id = 'listOfTerms'>
 `;
 
 // Checks if user is authenticated
 auth.onAuthStateChanged((user) =>
 { 
+    // If user is logged in
     if (user)
     {
+        // Sorts data alphabetically by term in ascending order
         fetchData('term', 'asc');
     }
+    // If user is not logged in
     else
     {
         // Redirects user if not logged in
@@ -82,7 +92,9 @@ auth.onAuthStateChanged((user) =>
 // Defines function for going through collection
 const createList = (data) =>
 {
+    // Counter used for giving elements unique IDs
     let counter = 1;
+    // Injectable HTML
     let html = '';
 
     // For each loop to cycle through the documents in the Collection
@@ -94,23 +106,31 @@ const createList = (data) =>
         // Temporary storage for the document's term
         const td = 
         `
+            <!-- Creates element containing term and definition -->
             <p id = ${counter} style = 'font-size: 19px; font-weight: 400; text-align: left; line-height: 40px;'><b><u>${term.term}</u></b> - ${term.definition}</p>
+            
+            <!-- Spacer -->
             <div style = 'padding: 10px;'></div>
         `;
 
         // Aggregates terms into single reference
         html += td;
+
+        // Increments counter
         counter += 1;
     })
 
-    // Injects HTML
+    // Closes previously open div containing list of terms
     html += `<div style = 'padding-bottom: 10%;'></div>`;
+
+    // Injects HTML
     terms.innerHTML = html;
 }
 
 // Refeshes data every time collection changes
 function fetchData(term, sort)
 {
+    // Sorts data depending on function arguments
     db.collection('terms').orderBy(term, sort).onSnapshot(snapshot =>
     {
         // Runs data through previously created function
@@ -121,14 +141,23 @@ function fetchData(term, sort)
 // Determines whether or not to display add term feature
 auth.onAuthStateChanged(user =>
 {
+    // If user is logged in
     if (user)
     {
+        // Creates blank injectable HTML
         let html = '';
+        // Esentially removes the Login button from the page, div element remains, though with dimensions of 0 
         loginButton.innerHTML = html;
+        // Creates more leftward space for icons to the right of where the login button used to be
         loginButton.style.margin = '-50px';
+
+        // Creates HTML to inject
         html =
         `
+            <!-- Logout button -->
             <button style = 
+
+            <!-- Element styling -->
             '
             margin-right: 4vw;
             background: transparent;
@@ -137,10 +166,13 @@ auth.onAuthStateChanged(user =>
             font-weight: 500;
             ' id = 'logout-button'>Logout</button>
         `;
+        // Injects HTML
         signupButton.innerHTML = html;
 
+        // Creates new constant to link to HTML element
         const logout = document.querySelector('#logout-button');
 
+        // Var storing if user is admin or standard
         var admin = false;
 
         // Checks if user is Admin
@@ -161,22 +193,29 @@ auth.onAuthStateChanged(user =>
 
             if (admin)
             {
+                // Creates injectable HTML
                 let html = 
                 `
+                    <!-- Form for admins to create new terms -->
                     <form action = 'dictionary.html' method = 'POST' id = 'new-term'>
                         <div id = 'term-def'>
+                            <!-- Term entry -->
                             <input type = 'text' placeholder = 'Term...' name = 'term' id = 'term' style = 'background: transparent;'></input>
+                            <!-- Definition entry -->
                             <input type = 'text' placeholder = 'Definition...' name = 'definition' id = 'definition' style = 'background: transparent;'></input>
                         </div>
     
+                        <!-- Spacer -->
                         <div id = 'small-gap'></div>
     
+                        <!-- Submit button -->
                         <button id = 'create-term'>Submit</button>
                     </form>
 
                     <br></br>
                 `;
     
+                // Injects HTML
                 createTerm.innerHTML = html;
             }
         })   
@@ -196,7 +235,6 @@ auth.onAuthStateChanged(user =>
         });
     }
 })
-
 
 // Submits term and definition to database
 createTerm.addEventListener('submit', (e) =>
@@ -219,148 +257,192 @@ createTerm.addEventListener('submit', (e) =>
 // Creates list of terms
 db.collection('terms').orderBy('term', 'asc').onSnapshot(snapshot =>
 {
+    // Counter to provide elements with unique IDs
     let counter = 1;
+    // Sets injectable HTML to previously defined variable containing HTML for the filter-by-letter buttons
     let html = searchTools;
 
+    // Iterates through collection
     snapshot.docs.forEach(doc =>
     {
+        // Stores document data in cosntant
         const information = doc.data();
 
+        // Creates injectable HTML
         const td = 
         `
             <a 
+            <!-- Link to actual term within search and filter box -->
             style = 'color: black; font-size: 18px; font-weight: 500;' href = '#${counter}'>${information.term}</a>
+            <!-- Spacer -->
             <div style = 'padding: 10px;'></div>
         `;
 
+        // Aggregates HTML
         html += td;
+        // Increments counter
         counter += 1;
     })
 
+    // Closes previously opened HTML
     html += `</div>`;
+    // Injects HTML
     listTerms.innerHTML = html;
 
+    // Creates constants linked to HTML elements
     const replaceTerms = document.querySelector('#listOfTerms');
     const searchTerm = document.querySelector('#search-bar');
 
+    // Listens to when something is searched in search bar
     searchTerm.addEventListener('submit', (e) =>
     {
+        // Prevents default action
         e.preventDefault();
             
-            const search = searchTerm['search'].value;
+        // Contains user-entered value from search bar
+        const search = searchTerm['search'].value;
 
-            let test = 'abdominoplasty';
-            console.log(test.includes(search.toString().toLowerCase()));
-
-            db.collection('terms').get().then(snapshot =>
+        // Retrieves data from 'terms' collection
+        db.collection('terms').get().then(snapshot =>
+        {
+            // Creates counter
+            let counter = 1;
+            // Creates injectable HTML
+            let html = '';
+            // Iterates through 'terms' collection
+            snapshot.docs.forEach(doc =>
             {
-                let counter = 1;
-                let html = '';
-                snapshot.docs.forEach(doc =>
-                {
-                    const information = doc.data();
+                // Stores document data in constant
+                const information = doc.data();
 
-                    if (information.term.toString().toLowerCase().includes(search.toString().toLowerCase()))
-                    {
-                        const td =
-                        `
-                            <a 
-                            style = 'color: black; font-size: 18px; font-weight: 500;' href = '#${counter}'>${information.term}</a>
-                            <div style = 'padding: 10px;'></div>
-                        `;
-                
-                        console.log('test: ' + counter);
-                        console.log(information.term.toString().toLowerCase());
-                        console.log(search.toString().toLowerCase());
-                        html += td;
-                    }
-                    counter += 1;
-                })
-                replaceTerms.innerHTML = html;
+                // Condition statement checking if user-searched term matches current document term
+                if (information.term.toString().toLowerCase().includes(search.toString().toLowerCase()))
+                {
+                    // If so, creates injectable HTML containing link to full definition within the Dictionary page for that word
+                    const td =
+                    `
+                        <a 
+                        style = 'color: black; font-size: 18px; font-weight: 500;' href = '#${counter}'>${information.term}</a>
+                        <div style = 'padding: 10px;'></div>
+                    `;
+            
+                    // Aggregates HTML
+                    html += td;
+                }
+                // Increments counter
+                counter += 1;
             })
+            // Injects HTML
+            replaceTerms.innerHTML = html;
+        })
     })
 })
 
+// Creates function for filtering terms when letter-button is pressed
 function sort(letter)
 {
+    // For Reset button, resets all filters
     if (letter === 'reset')
     {
 
     }
-
-    db.collection('terms').onSnapshot(snapshot =>
+    // If reset button wasn't pressed...
+    else
     {
-        // Runs data through previously created function
-        let counter = 1;
-        let termsHtml = '';
-        let listHtml = searchTools;
-        
-        snapshot.docs.forEach(doc =>
+        // Retreives data from 'terms' collection
+        db.collection('terms').onSnapshot(snapshot =>
         {
-            if (doc.id.toString().substring(0, 1) === letter)
-            {    
-                const term = 
-                `
-                    <p id = ${counter} style = 'font-size: 19px; font-weight: 400; text-align: left; line-height: 40px;'><b><u>${doc.data().term}</u></b> - ${doc.data().definition}</p>
-                    <div style = 'padding: 10px;'></div>
-                `;
-                termsHtml += term;
-
-                const link =
-                `
-                
-                    <a 
-                    style = 'color: black; font-size: 18px; font-weight: 500;' href = '#${counter}'>${doc.data().term}</a>
-                    <div style = 'padding: 10px;'></div>
-                `;
-                listHtml += link;
-            }
-
-            counter += 1;
-        })
-        listHtml += `</div>`;
-
-        terms.innerHTML = termsHtml;
-        listTerms.innerHTML = listHtml;
-
-        const replaceTerms = document.querySelector('#listOfTerms');
-        const searchTerm = document.querySelector('#search-bar');
-
-        searchTerm.addEventListener('submit', (e) =>
-        {
-            e.preventDefault();
+            // Creates counter
+            let counter = 1;
+            // Initializes injectable HTML
+            let termsHtml = '';
+            // Sets existing search tools to blank HTML to be updated later
+            let listHtml = searchTools;
             
-            const search = searchTerm['search'].value;
-
-            let test = 'abdominoplasty';
-            console.log(test.includes(search.toString().toLowerCase()));
-
-            db.collection('terms').get().then(snapshot =>
+            // Iterates through collection
+            snapshot.docs.forEach(doc =>
             {
-                let counter = 1;
-                let html = '';
-                snapshot.docs.forEach(doc =>
-                {
-                    const information = doc.data();
+                // Condition statement checking if ANY PART of the search is contained within the document name
+                if (doc.id.toString().substring(0, 1) === letter)
+                {    
+                    // Creates injectable HTML for definition
+                    const term = 
+                    `
+                        <p id = ${counter} style = 'font-size: 19px; font-weight: 400; text-align: left; line-height: 40px;'><b><u>${doc.data().term}</u></b> - ${doc.data().definition}</p>
+                        <div style = 'padding: 10px;'></div>
+                    `;
+                    // Aggregates HTML
+                    termsHtml += term;
 
-                    if (information.term.toString().toLowerCase().includes(search.toString().toLowerCase()))
-                    {
-                        const td =
-                        `
-                            <a 
-                            style = 'color: black; font-size: 18px; font-weight: 500;' href = '#${counter}'>${information.term}</a>
-                            <div style = 'padding: 10px;'></div>
-                        `;
+                    // Creates injectable HTML for fast-access link to full-definition
+                    const link =
+                    `
+                    
+                        <a 
+                        style = 'color: black; font-size: 18px; font-weight: 500;' href = '#${counter}'>${doc.data().term}</a>
+                        <div style = 'padding: 10px;'></div>
+                    `;
+                    // Aggregates HTML
+                    listHtml += link;
+                }
+
+                // Increments counter
+                counter += 1;
+            })
+            // Closes previously opened div
+            listHtml += `</div>`;
+
+            // Injects HTML
+            terms.innerHTML = termsHtml;
+            listTerms.innerHTML = listHtml;
+
+            // Creates new constants linked to HTML elements created by injectable code
+            const replaceTerms = document.querySelector('#listOfTerms');
+            const searchTerm = document.querySelector('#search-bar');
+
+            // Detects if search was entered
+            searchTerm.addEventListener('submit', (e) =>
+            {
+                // Prevents default action
+                e.preventDefault();
                 
-                        console.log('test: ' + counter);
-                        console.log(information.term.toString().toLowerCase());
-                        console.log(search.toString().toLowerCase());
-                        html += td;
-                    }
-                    counter += 1;
+                // Stores user-search term in constant
+                const search = searchTerm['search'].value;
+
+                // Retrieves data from 'terms' collection
+                db.collection('terms').get().then(snapshot =>
+                {
+                    // Creates counter to provide elements with unique IDs
+                    let counter = 1;
+                    // Initializes HTML inject
+                    let html = '';
+                    // Iterates through documents in collection
+                    snapshot.docs.forEach(doc =>
+                    {
+                        // Stores document data
+                        const information = doc.data();
+
+                        // Condition statement checking if ANY PART of the document term contains the user-searched value
+                        if (information.term.toString().toLowerCase().includes(search.toString().toLowerCase()))
+                        {
+                            // Initializes injectable HTML
+                            const td =
+                            `
+                                <a 
+                                style = 'color: black; font-size: 18px; font-weight: 500;' href = '#${counter}'>${information.term}</a>
+                                <div style = 'padding: 10px;'></div>
+                            `;
+                    
+                            // Aggregates HTML
+                            html += td;
+                        }
+                        // Increments counter
+                        counter += 1;
+                    })
+                    // Injects HTML
+                    replaceTerms.innerHTML = html;
                 })
-                replaceTerms.innerHTML = html;
             })
         })
-    })
+    }
 }
